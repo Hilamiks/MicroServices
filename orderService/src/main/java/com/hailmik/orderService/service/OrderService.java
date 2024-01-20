6b0442fd21bfc9659bac8d1c891fe3fa31e44e90
Mike Sokolov <miha13.sokolov@gmail.com>
2024-01-20 21:53:47 +0500

    communication between order and inventory finished

--- orderService/src/main/java/com/hailmik/orderService/service/OrderService.java
+++ orderService/src/main/java/com/hailmik/orderService/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.hailmik.orderService.service;
 
+import com.hailmik.orderService.dto.InventoryResponse;
 import com.hailmik.orderService.dto.OrderLineItemsDto;
 import com.hailmik.orderService.dto.OrderRequest;
 import com.hailmik.orderService.model.Order;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -34,14 +36,24 @@ public class OrderService {
 
 		order.setOrderLineItemsList(orderLineItemsList);
 
+		List<String> skuCodes = order.getOrderLineItemsList()
+				.stream()
+				.map(OrderLineItems::getSkuCode)
+				.toList();
 		//Call inventory service and place order if the product is in stock
-		Boolean result = webClient.get()
-				.uri("http://localhost:8082/api/inventory")
+		InventoryResponse[] inventoryResponseArray = webClient.get()
+				.uri(
+						"http://localhost:8082/api/inventory",
+						uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build()
+						)
 				.retrieve()
-				.bodyToMono(Boolean.class)
+				.bodyToMono(InventoryResponse[].class)
 				.block();
 
-		if (result) {
+		boolean allProductsInStock = Arrays.stream(inventoryResponseArray).allMatch(InventoryResponse::isInStock);
+
+
+		if (allProductsInStock) {
 			orderRepository.save(order);
 		} else {
 			throw new IllegalAccessException("Product is not in stock, please try again later");
